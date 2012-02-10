1b7e0229c2c04506157b46e21ba2f1988b611803
Kanzhe Jiang <kanzhe.jiang@bigswitch.com>
2012-02-09 18:27:50 -0800

    Change the networkAddr and AP aging removal code to correctly update all deviceMaps.

--- src/main/java/net/floodlightcontroller/devicemanager/internal/DeviceManagerImpl.java
+++ src/main/java/net/floodlightcontroller/devicemanager/internal/DeviceManagerImpl.java
@@ -199,6 +199,10 @@ public class DeviceManagerImpl implements IDeviceManager, IOFMessageListener,
             }
         }
 
+        private void delFromIpv4AddressDeviceMap(Integer ip, Device d) {
+        	ipv4AddressDeviceMap.remove(ip);
+        }
+        
         private void updateSwitchDeviceMap(Device d) {
             // Find all the attachment points of this device
             // Then find all the switches from the attachment points
@@ -404,6 +408,26 @@ public class DeviceManagerImpl implements IDeviceManager, IOFMessageListener,
             }
         }
 
+        /**
+         * Delete a network address from a device
+         * @param dlAddr The link layer address of the device
+         * @param nwAddr the new network layer address
+         */
+        protected void delNwAddrByDataLayerAddr(long dlAddr,
+        									int nwAddr) {
+        	Device d = getDeviceByDataLayerAddr(dlAddr);
+            if (d == null) return;
+            Device dCopy = new Device(d);
+            DeviceNetworkAddress na = dCopy.getNetworkAddress(nwAddr);
+            
+            if (na != null) {
+            	delFromIpv4AddressDeviceMap(nwAddr, d);
+                dCopy.removeNetworkAddress(na);
+                updateMaps(dCopy);
+            }
+            d = null; // to catch if anyone is using this reference
+        }
+        
         /**
          * Add a device attachment point to the device with the given
          * link-layer address
@@ -1601,7 +1625,8 @@ public class DeviceManagerImpl implements IDeviceManager, IOFMessageListener,
             Date agedBoundary = ageBoundaryDifference(currentDate, expire);
 
             if (address.getLastSeen().before(agedBoundary)) {
-                device.removeNetworkAddress(address.getNetworkAddress());
+            	devMgrMaps.delNwAddrByDataLayerAddr(device.getDataLayerAddressAsLong(), 
+            			address.getNetworkAddress().intValue());
             }
         }
     }
@@ -1617,7 +1642,7 @@ public class DeviceManagerImpl implements IDeviceManager, IOFMessageListener,
             }
             Date agedBoundary = ageBoundaryDifference(currentDate, expire);
             if (ap.getLastSeen().before(agedBoundary)) {
-                device.removeAttachmentPoint(ap.getSwitchPort());
+                devMgrMaps.delDevAttachmentPoint(device, ap.getSwitchPort());
             }
         }
     }
