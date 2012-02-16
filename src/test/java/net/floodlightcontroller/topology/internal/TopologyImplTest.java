81c81e61009a6066a7cd3c604d2dea7a7541a3af
Subrata Banerjee <subrata.banerjee@bigswitch.com>
2012-02-15 21:18:19 -0800

    Enhanced debug stmt for link removal event.

--- src/test/java/net/floodlightcontroller/topology/internal/TopologyImplTest.java
+++ src/test/java/net/floodlightcontroller/topology/internal/TopologyImplTest.java
@@ -23,7 +23,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
