f99efa127d55b5d5f020cf620c19118cbd5a57f9
Alex Reimers <alex@bigswitch.com>
2012-03-07 12:08:33 -0800

    Add Null/ClassCastException checks to the event history REST APIs.

--- src/main/java/net/floodlightcontroller/core/web/EventHistoryPacketInResource.java
+++ src/main/java/net/floodlightcontroller/core/web/EventHistoryPacketInResource.java
@@ -10,17 +10,20 @@ import net.floodlightcontroller.util.EventHistory;
 import org.openflow.protocol.OFMatch;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author subrata
  *
  */
 public class EventHistoryPacketInResource extends ServerResource {
-
+    // TODO - Move this to the DeviceManager rest API
+    protected static Logger log = 
+            LoggerFactory.getLogger(EventHistoryPacketInResource.class);
+    
     @Get("json")
     public EventHistory<OFMatch> handleEvHistReq() {
-
-        EventHistory<OFMatch> ofm = null;
         // Get the count (number of latest events requested), zero means all
         String evHistCount = (String)getRequestAttributes().get("count");
         int    count = EventHistory.EV_HISTORY_DEFAULT_SIZE;
@@ -31,12 +34,19 @@ public class EventHistoryPacketInResource extends ServerResource {
             // Invalid input for event count - use default value
         }
 
-        DeviceManagerImpl deviceManager = 
-           (DeviceManagerImpl)getContext().getAttributes().
-               get(IDeviceManagerService.class.getCanonicalName());
-
-        ofm =  new EventHistory<OFMatch>(deviceManager.evHistDevMgrPktIn,count);
-        return ofm;
+        try {
+            DeviceManagerImpl deviceManager = 
+               (DeviceManagerImpl)getContext().getAttributes().
+                   get(IDeviceManagerService.class.getCanonicalName());
+    
+            if (deviceManager != null) {
+                return new EventHistory<OFMatch>(deviceManager.evHistDevMgrPktIn,count);
+            }
+        } catch (ClassCastException e) {
+            log.error(e.toString());
+        }
+        
+        return null;
     }
 
 }
