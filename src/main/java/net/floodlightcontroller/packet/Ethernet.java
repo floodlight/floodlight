23862a21062bb2550fe12c99fa4694dbaf4db322
Shudong Zhou <shudongzhou@gmail.com>
2012-03-16 21:45:24 -0700

    Add BSN probe packet

--- src/main/java/net/floodlightcontroller/packet/Ethernet.java
+++ src/main/java/net/floodlightcontroller/packet/Ethernet.java
@@ -34,6 +34,7 @@ public class Ethernet extends BasePacket {
     public static final short TYPE_ARP = 0x0806;
     public static final short TYPE_IPv4 = 0x0800;
     public static final short TYPE_LLDP = (short) 0x88cc;
+    public static final short TYPE_BSN = (short) 0x8950; // Not officially assigned
     public static final short VLAN_UNTAGGED = (short)0xffff;
     public static final short TYPE_BDDP = (short) 0x01ab;
     public static Map<Short, Class<? extends IPacket>> etherTypeClassMap;
@@ -44,6 +45,7 @@ public class Ethernet extends BasePacket {
         etherTypeClassMap.put(TYPE_IPv4, IPv4.class);
         etherTypeClassMap.put(TYPE_LLDP, LLDP.class);
         etherTypeClassMap.put(TYPE_BDDP, BDDP.class);
+        etherTypeClassMap.put(TYPE_BSN, BSN.class);
     }
 
     protected MACAddress destinationMACAddress;
