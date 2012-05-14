4a5ca781ebe985c079fc1820b6484ed4ecfae6bb
Rich Lane <rich.lane@bigswitch.com>
2012-05-14 15:54:21 -0700

    debian packaging: fix lintian warnings

--- debian/init.d
+++ debian/init.d
@@ -1,8 +1,8 @@
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides:          floodlight
-# Required-Start:    $network $local_fs
-# Required-Stop:
+# Required-Start:    $network $local_fs $remote_fs
+# Required-Stop:     $remote_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: An Apache licensed, Java based OpenFlow controller
