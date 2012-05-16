0af960b7d1e0de0fe89827bfc778ea6e4ab8e50c
Rob Adams <rob.adams@bigswitch.com>
2012-05-15 18:18:33 -0700

    Fix warning by removing unused import

--- src/test/java/net/floodlightcontroller/core/internal/ControllerTest.java
+++ src/test/java/net/floodlightcontroller/core/internal/ControllerTest.java
@@ -38,7 +38,6 @@ import net.floodlightcontroller.core.IHAListener;
 import net.floodlightcontroller.core.IFloodlightProviderService.Role;
 import net.floodlightcontroller.core.IOFMessageFilterManagerService;
 import net.floodlightcontroller.core.IOFMessageListener;
-import net.floodlightcontroller.core.IFloodlightProviderService.Role;
 import net.floodlightcontroller.core.IOFMessageListener.Command;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.IOFSwitchListener;
