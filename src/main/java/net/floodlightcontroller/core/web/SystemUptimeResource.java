95034ba2de5bd4bb670a8f57875f7a0db85d5904
Munish Mehta <munish.mehta@bigswitch.com>
2012-06-22 13:08:49 -0700

    minor syntax change in the REST return value

--- src/main/java/net/floodlightcontroller/core/web/SystemUptimeResource.java
+++ src/main/java/net/floodlightcontroller/core/web/SystemUptimeResource.java
@@ -4,13 +4,28 @@ import net.floodlightcontroller.core.IFloodlightProviderService;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
 
+
+
 public class SystemUptimeResource extends ServerResource {
+	
+	public class UptimeRest {
+		long systemUptimeMsec;
+
+		public long getSystemUptimeMsec() {
+			return systemUptimeMsec;
+		}
+	}
+	
 	@Get("json")
-	public long retrieve() {
+	public UptimeRest retrieve() {
 		IFloodlightProviderService floodlightProvider = 
 			(IFloodlightProviderService)getContext().getAttributes().
 			get(IFloodlightProviderService.class.getCanonicalName());
-		return (System.currentTimeMillis() -
-				floodlightProvider.getSystemStartTime());
+		
+		UptimeRest uptime = new UptimeRest();
+		uptime.systemUptimeMsec = 
+		   System.currentTimeMillis() - floodlightProvider.getSystemStartTime();
+		
+		return (uptime);
 	}
 }
