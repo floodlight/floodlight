abc022081aeb31c096d6252dfaf1c7ede411b3a4
Colin Dixon <ckd@us.ibm.com>
2012-07-03 16:59:01 -0500

    making it possible to get LinkType information both from LinkInfo.getLinkType() and the REST API

--- src/main/java/net/floodlightcontroller/linkdiscovery/web/LinksResource.java
+++ src/main/java/net/floodlightcontroller/linkdiscovery/web/LinksResource.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryService;
+import net.floodlightcontroller.linkdiscovery.LinkInfo;
 import net.floodlightcontroller.linkdiscovery.LinkTuple;
 
 import org.restlet.resource.Get;
@@ -17,7 +18,12 @@ public class LinksResource extends ServerResource {
         Set <LinkTuple> links = new HashSet<LinkTuple>();
         if (topo != null) {
             for (Set<LinkTuple> linkSet : topo.getSwitchLinks().values()) {
-                links.addAll(linkSet);
+                for (LinkTuple lt : linkSet) {
+                    LinkInfo info = topo.getLinkInfo(lt.getSrc(), true);
+                    LinkTuple withType = new LinkTuple(lt.getSrc(), lt.getDst());
+                    withType.setType(LinkInfo.getLinkType(lt, info));
+                    links.add(withType);
+                }
             }
         }
         return links;
