f83c9ef65ba2f50db8993e0fdc69f5f88b0c84e4
Srinivasan Ramasubramanian <srini@bigswitch.com>
2012-07-22 11:27:41 -0700

    Bug fix: prevCluster assignment is changed to reflect the L2 domain ID of the prev entity.  It was possible for cur and prev to be swapped when duplicate attachment points were detected, where the prevCluster could have the wrong assignment.

--- src/main/java/net/floodlightcontroller/devicemanager/internal/Device.java
+++ src/main/java/net/floodlightcontroller/devicemanager/internal/Device.java
@@ -332,7 +332,7 @@ public class Device implements IDevice {
             }
             
             prev = clentities[clEntIndex] = cur;
-            prevCluster = curCluster;
+            prevCluster = topology.getL2DomainId(prev.getSwitchDPID());
 
             long prevLastSeen = 
                     deviceManager.apComparator.
