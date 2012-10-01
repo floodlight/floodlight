8fef1e7bc62c5c678e16b8739bca3aefcadb9f85
KAWAI Hiroaki <kawai@stratosphere.co.jp>
2012-10-01 18:20:15 +0900

    Add jsvc compatible interface.
    jsvc http://commons.apache.org/daemon/jsvc.html is a handy
    helper tool to create a regular init script to run.
    With this patch, we can run like this:
    /usr/bin/jsvc \
     -cp lib/*.jar:target/floodlight.jar net.floodlightcontroller.core.Main

--- src/main/java/net/floodlightcontroller/core/Main.java
+++ src/main/java/net/floodlightcontroller/core/Main.java
@@ -14,36 +14,55 @@ import net.floodlightcontroller.restserver.IRestApiService;
  * @author alexreimers
  */
 public class Main {
-
+	static CmdLineSettings settings = new CmdLineSettings();
+	static CmdLineParser parser = new CmdLineParser(settings);
+	
+	IRestApiService restApi = null;
+	IFloodlightProviderService controller = null;
+	
     /**
      * Main method to load configuration and modules
      * @param args
      * @throws FloodlightModuleException 
      */
     public static void main(String[] args) throws FloodlightModuleException {
+    	Main boot = new Main();
+    	try{
+	    	boot.init(args);
+    	}catch(CmdLineException e){
+            parser.printUsage(System.out);
+            System.exit(1);
+    	}
+    }
+    
+    public void init(String[] args) throws CmdLineException,FloodlightModuleException {
         // Setup logger
         System.setProperty("org.restlet.engine.loggerFacadeClass", 
                 "org.restlet.ext.slf4j.Slf4jLoggerFacade");
         
-        CmdLineSettings settings = new CmdLineSettings();
-        CmdLineParser parser = new CmdLineParser(settings);
-        try {
-            parser.parseArgument(args);
-        } catch (CmdLineException e) {
-            parser.printUsage(System.out);
-            System.exit(1);
-        }
+        parser.parseArgument(args);
         
         // Load modules
         FloodlightModuleLoader fml = new FloodlightModuleLoader();
         IFloodlightModuleContext moduleContext = fml.loadModulesFromConfig(settings.getModuleFile());
         // Run REST server
-        IRestApiService restApi = moduleContext.getServiceImpl(IRestApiService.class);
-        restApi.run();
+        restApi = moduleContext.getServiceImpl(IRestApiService.class);
         // Run the main floodlight module
-        IFloodlightProviderService controller =
-                moduleContext.getServiceImpl(IFloodlightProviderService.class);
+        controller = moduleContext.getServiceImpl(IFloodlightProviderService.class);
+    }
+    
+    public void start() {
+        restApi.run();
         // This call blocks, it has to be the last line in the main
-        controller.run();
+        controller.run();    	
+    }
+    
+    public void stop() {
+    	// no-op because IRestApiService don't have stop()
+    	// op-op because IFloodlightProviderService don't have stop()
+    }
+    
+    public void destroy() {
+    	
     }
 }
