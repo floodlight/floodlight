902407f3f0a8b0cdc9348680b61369b8fd807305
Srinivasan Ramasubramanian <srini@bigswitch.com>
2012-10-18 02:32:59 -0700

    Changes to REST API for links.  http://<controller>:8080/wm/topology/links/json would now return only direct and tunnel links (LinkType.DIRECT_LINK or LinkType.TUNNEL).  To get external links, use http://<controller>:8080/wm/topology/external-links/json.

--- src/main/java/net/floodlightcontroller/topology/web/TopologyWebRoutable.java
+++ src/main/java/net/floodlightcontroller/topology/web/TopologyWebRoutable.java
@@ -3,6 +3,7 @@ package net.floodlightcontroller.topology.web;
 import org.restlet.Context;
 import org.restlet.routing.Router;
 
+import net.floodlightcontroller.linkdiscovery.web.ExternalLinksResource;
 import net.floodlightcontroller.linkdiscovery.web.LinksResource;
 import net.floodlightcontroller.restserver.RestletRoutable;
 
@@ -14,6 +15,7 @@ public class TopologyWebRoutable implements RestletRoutable {
     public Router getRestlet(Context context) {
         Router router = new Router(context);
         router.attach("/links/json", LinksResource.class);
+        router.attach("/external-links/json", ExternalLinksResource.class);
         router.attach("/tunnellinks/json", TunnelLinksResource.class);
         router.attach("/switchclusters/json", SwitchClustersResource.class);
         router.attach("/broadcastdomainports/json", BroadcastDomainPortsResource.class);
