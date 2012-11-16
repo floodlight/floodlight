d80c5557c53056b1df82681599726ca92b6a8cda
Andreas Wundsam <andreas.wundsam@bigswitch.com>
2012-11-15 16:50:35 -0800

    openflow/j: Factor out Nicira Vendor Extensions initialization to util class

--- src/main/java/net/floodlightcontroller/core/internal/Controller.java
+++ src/main/java/net/floodlightcontroller/core/internal/Controller.java
@@ -1767,21 +1767,9 @@ public class Controller implements IFloodlightProviderService,
     private void initVendorMessages() {
         // Configure openflowj to be able to parse the role request/reply
         // vendor messages.
-        OFBasicVendorId niciraVendorId = new OFBasicVendorId(
-                OFNiciraVendorData.NX_VENDOR_ID, 4);
-        OFVendorId.registerVendorId(niciraVendorId);
-        OFBasicVendorDataType roleRequestVendorData =
-                new OFBasicVendorDataType(
-                        OFRoleRequestVendorData.NXT_ROLE_REQUEST,
-                        OFRoleRequestVendorData.getInstantiable());
-        niciraVendorId.registerVendorDataType(roleRequestVendorData);
-        OFBasicVendorDataType roleReplyVendorData =
-                new OFBasicVendorDataType(
-                        OFRoleReplyVendorData.NXT_ROLE_REPLY,
-                        OFRoleReplyVendorData.getInstantiable());
-         niciraVendorId.registerVendorDataType(roleReplyVendorData);
+        OFNiciraVendorExtensions.initialize();
     }
-    
+
     /**
      * Initialize internal data structures
      */
