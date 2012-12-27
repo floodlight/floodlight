930b4faad158ede757a3f8991e3da4f43940ea58
Jason Parraga <Sovietaced@gmail.com>
2012-12-27 16:08:41 -0500

    Overwrote the default JSON serializer to print IP addresses as strings instead of integers

--- src/main/java/net/floodlightcontroller/firewall/FirewallRule.java
+++ src/main/java/net/floodlightcontroller/firewall/FirewallRule.java
@@ -1,5 +1,7 @@
 package net.floodlightcontroller.firewall;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
 import org.openflow.protocol.OFMatch;
 
 import net.floodlightcontroller.packet.Ethernet;
@@ -8,6 +10,7 @@ import net.floodlightcontroller.packet.IPv4;
 import net.floodlightcontroller.packet.TCP;
 import net.floodlightcontroller.packet.UDP;
 
+@JsonSerialize(using=FirewallRuleSerializer.class)
 public class FirewallRule implements Comparable<FirewallRule> {
     public int ruleid;
 
