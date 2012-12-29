213c8b88c34297efa00e613e9c9375b3be257ba4
Martin Fong <mwfong@comcast.net>
2012-12-29 11:49:43 -0800

    Producer/consumer registration service.

---
+++ src/main/java/org/openflow/util/IProducer.java
@@ -0,0 +1,8 @@
+package org.openflow.util;
+
+public interface IProducer {
+
+	public void registerConsumer   (Class<?> iface, Object anObj);
+	public void deregisterConsumer (Class<?> iface, Object anObj);
+
+}
