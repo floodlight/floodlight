201273969a217420b8930edc169baeb0bfaf47a9
kwanggithub <kwang@clemson.edu>
2013-01-02 21:25:53 -0800

    fix tabs

--- src/main/java/org/openflow/util/IProducer.java
+++ src/main/java/org/openflow/util/IProducer.java
@@ -2,7 +2,8 @@ package org.openflow.util;
 
 public interface IProducer {
 
-	public void registerConsumer   (Class<?> iface, Object anObj);
-	public void deregisterConsumer (Class<?> iface, Object anObj);
+    public void registerConsumer(Class<?> iface, Object anObj);
+
+    public void deregisterConsumer(Class<?> iface, Object anObj);
 
 }
