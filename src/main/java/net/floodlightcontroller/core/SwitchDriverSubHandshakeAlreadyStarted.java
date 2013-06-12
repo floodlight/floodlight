93bd1935a28cc156dbe0401c3f397a39dd0edddc
Gregor Maier <gregor.maier@bigswitch.com>
2013-06-12 16:24:15 -0700

    more unit tests and javadocs

--- src/main/java/net/floodlightcontroller/core/SwitchDriverSubHandshakeAlreadyStarted.java
+++ src/main/java/net/floodlightcontroller/core/SwitchDriverSubHandshakeAlreadyStarted.java
@@ -1,5 +1,10 @@
 package net.floodlightcontroller.core;
 
+/**
+ * Thrown when IOFSwitch.startDriverHandshake() is called more than once.
+ * @author gregor
+ *
+ */
 public class SwitchDriverSubHandshakeAlreadyStarted extends
     SwitchDriverSubHandshakeException {
     private static final long serialVersionUID = -5491845708752443501L;
--- src/main/java/net/floodlightcontroller/core/SwitchDriverSubHandshakeNotStarted.java
+++ src/main/java/net/floodlightcontroller/core/SwitchDriverSubHandshakeNotStarted.java
@@ -1,5 +1,11 @@
 package net.floodlightcontroller.core;
 
+/**
+ * Thrown when a switch driver's sub-handshake has not been started but an
+ * operation requiring the sub-handshake has been attempted.
+ * @author gregor
+ *
+ */
 public class SwitchDriverSubHandshakeNotStarted extends
     SwitchDriverSubHandshakeException {
     private static final long serialVersionUID = -5491845708752443501L;
