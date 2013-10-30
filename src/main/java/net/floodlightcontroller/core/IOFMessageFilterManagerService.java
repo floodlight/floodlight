d7b77258cf945ba8dc0e576e5040a523fb45324f
Sovietaced <Sovietaced@gmail.com>
2013-10-30 14:52:51 -0700

    Fix packet tracer API

--- src/main/java/net/floodlightcontroller/core/IOFMessageFilterManagerService.java
+++ src/main/java/net/floodlightcontroller/core/IOFMessageFilterManagerService.java
@@ -16,8 +16,13 @@
 
 package net.floodlightcontroller.core;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import net.floodlightcontroller.core.module.IFloodlightService;
 
 public interface IOFMessageFilterManagerService extends IFloodlightService {
+
+	String setupFilter(String sid, ConcurrentHashMap<String, String> f,
+			int deltaInMilliSeconds);
     // empty for now
 }
