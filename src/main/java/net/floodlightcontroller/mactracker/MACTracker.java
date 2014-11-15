949b28b13f28204bcbf881e4302332e2a6631df5
Ankit Agrawal <ankitagrawal.in@gmail.com>
2014-11-14 19:41:27 -0500

    MACTracker module from basic Floodlight Tutorial

---
+++ src/main/java/net/floodlightcontroller/mactracker/MACTracker.java
@@ -0,0 +1,135 @@
+/**
+ * 
+ */
+package net.floodlightcontroller.mactracker;
+
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.Set;
+import net.floodlightcontroller.packet.Ethernet;
+import org.openflow.util.HexString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.openflow.protocol.OFMessage;
+import org.openflow.protocol.OFType;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+
+
+/**
+ * @author aagrawa5
+ *
+ */
+public class MACTracker implements IOFMessageListener, IFloodlightModule {
+	
+	protected IFloodlightProviderService floodlightProvider;
+	protected Set macAddresses;
+	protected static Logger logger;
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.IListener#getName()
+	 */
+	@Override
+	public String getName() {
+		return MACTracker.class.getSimpleName();
+	}
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.IListener#isCallbackOrderingPrereq(java.lang.Object, java.lang.String)
+	 */
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.IListener#isCallbackOrderingPostreq(java.lang.Object, java.lang.String)
+	 */
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#getModuleServices()
+	 */
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#getServiceImpls()
+	 */
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#getModuleDependencies()
+	 */
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		Collection<Class<? extends IFloodlightService>> l =
+				new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		return l;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#init(net.floodlightcontroller.core.module.FloodlightModuleContext)
+	 */
+	@Override
+	public void init(FloodlightModuleContext context)
+			throws FloodlightModuleException {
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		macAddresses = new ConcurrentSkipListSet<Long>();
+		logger = LoggerFactory.getLogger(MACTracker.class);
+	}
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.module.IFloodlightModule#startUp(net.floodlightcontroller.core.module.FloodlightModuleContext)
+	 */
+	@Override
+	public void startUp(FloodlightModuleContext context)
+			throws FloodlightModuleException {
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+	}
+
+	/* (non-Javadoc)
+	 * @see net.floodlightcontroller.core.IOFMessageListener#receive(net.floodlightcontroller.core.IOFSwitch, org.openflow.protocol.OFMessage, net.floodlightcontroller.core.FloodlightContext)
+	 */
+	@Override
+	public net.floodlightcontroller.core.IListener.Command receive(
+			IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		Ethernet eth =
+				IFloodlightProviderService.bcStore.get(cntx,
+						IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+
+		Long sourceMACHash = Ethernet.toLong(eth.getSourceMACAddress());
+		if (!macAddresses.contains(sourceMACHash)) {
+			macAddresses.add(sourceMACHash);
+			logger.info("MAC Address: {} seen on switch: {}",
+					HexString.toHexString(sourceMACHash),
+					sw.getId());
+		}
+		return Command.CONTINUE;
+	}
+
+}
