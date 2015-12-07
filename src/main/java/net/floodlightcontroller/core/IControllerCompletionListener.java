947bebfb7bb9c4f43556ecb498b6f427f6d3be83
paaguti-work <pedroa.aranda@telefonica.com>
2015-12-07 16:19:20 +0100

    Include missing file

---
+++ src/main/java/net/floodlightcontroller/core/IControllerCompletionListener.java
@@ -0,0 +1,21 @@
+package net.floodlightcontroller.core;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+// import net.floodlightcontroller.core.IListener.Command;
+
+public interface IControllerCompletionListener {
+
+	/**
+	 * This mimics the  behaviour of the IOFMessageListener. Will be called at the end of the message processing loop
+	 * Modules implementing this interface will know when the message processing queue has digested an input event
+	 * 
+	 * @param sw
+	 * @param msg
+	 * @param cntx
+	 *
+	 */
+	public void onMessageConsumed(IOFSwitch sw, OFMessage msg, FloodlightContext cntx);
+
+	public String getName();
+	
+}
