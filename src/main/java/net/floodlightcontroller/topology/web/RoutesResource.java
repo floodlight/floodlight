f0ce6eb84280e631029008aff350cda92b12543c
Geddings Barrineau <cbarrin@g.clemson.edu>
2016-07-13 15:17:56 -0400

    Testing out some things related to the REST api.

--- src/main/java/net/floodlightcontroller/topology/web/RoutesResource.java
+++ src/main/java/net/floodlightcontroller/topology/web/RoutesResource.java
@@ -41,9 +41,8 @@ public class RoutesResource extends ServerResource {
         String dstDpid = (String) getRequestAttributes().get("dst-dpid");
         Integer numRoutes = Integer.parseInt((String) getRequestAttributes().get("num-routes"));
 
-        log.info("REQUEST: {}", getRequest().getRootRef().toString());
-        log.info("REQUEST: {}", getRequest().getResourceRef().toString());
-        log.info("REQUEST: {}", getContext().getAttributes().get("contextPath").toString());
+        log.info("REQUEST1: {}", getRequest().getRootRef().toString());
+        log.info("REQUEST2: {}", getRequest().getResourceRef().toString());
 
         log.debug("Asking for routes from {} to {}", srcDpid, dstDpid);
         log.debug("Asking for {} routes", numRoutes);
