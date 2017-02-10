bca26cb56c5e5b751e1e2bf4a9bed54c5751809f
Bhargav Srinivasan <bhargavsvasan@hotmail.com>
2017-02-10 03:10:06 -0500

    ZMQ retired, Netty 4 rises again

--- src/test/java/net/floodlightcontroller/hasupport/TestClient.java
+++ src/test/java/net/floodlightcontroller/hasupport/TestClient.java
@@ -3,41 +3,40 @@ package net.floodlightcontroller.hasupport;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import org.zeromq.ZMQ;
 
 @Ignore 
 public class TestClient {
 	
-	String mockClientPort = new String();
-	ZMQ.Context zmqcontext = ZMQ.context(1);
-	ZMQ.Socket requester = zmqcontext.socket(ZMQ.REQ);
-	
-	public TestClient(String clientPort){
-		this.mockClientPort = clientPort;
-		requester.connect("tcp://"+mockClientPort);
-		System.out.println("Starting client...");
-	}
-
-	public String send(String message){
-		try{ 
-			requester.send(message.getBytes(),0);
-			byte[] response = requester.recv();
-			System.out.println("Server Response: " + new String(response));		
-			return new String(response);
-			
-		} catch (Exception e){
-			System.out.println(e);
-			requester.close();
-			zmqcontext.term();
-			
-		}
-		return new String("none");
-	}
-	
-	@Test
-	public void nothing() {
-		return;
-	}
+//	String mockClientPort = new String();
+//	ZMQ.Context zmqcontext = ZMQ.context(1);
+//	ZMQ.Socket requester = zmqcontext.socket(ZMQ.REQ);
+//	
+//	public TestClient(String clientPort){
+//		this.mockClientPort = clientPort;
+//		requester.connect("tcp://"+mockClientPort);
+//		System.out.println("Starting client...");
+//	}
+//
+//	public String send(String message){
+//		try{ 
+//			requester.send(message.getBytes(),0);
+//			byte[] response = requester.recv();
+//			System.out.println("Server Response: " + new String(response));		
+//			return new String(response);
+//			
+//		} catch (Exception e){
+//			System.out.println(e);
+//			requester.close();
+//			zmqcontext.term();
+//			
+//		}
+//		return new String("none");
+//	}
+//	
+//	@Test
+//	public void nothing() {
+//		return;
+//	}
 
 
 }
--- src/test/java/net/floodlightcontroller/hasupport/TestServer.java
+++ src/test/java/net/floodlightcontroller/hasupport/TestServer.java
@@ -1,39 +1,44 @@
 package net.floodlightcontroller.hasupport;
 
 import org.junit.Ignore;
-import org.zeromq.ZMQ;
 
 @Ignore
 public class TestServer implements Runnable {
-	
-	String mockServerPort = new String();
-	ZMQ.Context zmqcontext = ZMQ.context(1);
-	ZMQ.Socket responder = zmqcontext.socket(ZMQ.REP);
-	String reply = new String("ACK");
-	
-	public TestServer(String serverPort){
-		this.mockServerPort = serverPort;
-		responder.bind("tcp://"+mockServerPort);
-		System.out.println("Starting client...");
-	}
 
+	@Override
 	public void run() {
-		try{
-			while (!Thread.currentThread().isInterrupted()) {
-				byte[] response = responder.recv(0);
-				System.out.println("Server Got: " + new String(response));
-				responder.send(reply);
-			}
-			
-		} catch (Exception e){
-			e.printStackTrace();
-			responder.close();
-			zmqcontext.term();
-			
-		}
-		responder.close();
-		zmqcontext.term();
+		// TODO Auto-generated method stub
+		
 	}
+	
+//	String mockServerPort = new String();
+//	ZMQ.Context zmqcontext = ZMQ.context(1);
+//	ZMQ.Socket responder = zmqcontext.socket(ZMQ.REP);
+//	String reply = new String("ACK");
+//	
+//	public TestServer(String serverPort){
+//		this.mockServerPort = serverPort;
+//		responder.bind("tcp://"+mockServerPort);
+//		System.out.println("Starting client...");
+//	}
+//
+//	public void run() {
+//		try{
+//			while (!Thread.currentThread().isInterrupted()) {
+//				byte[] response = responder.recv(0);
+//				System.out.println("Server Got: " + new String(response));
+//				responder.send(reply);
+//			}
+//			
+//		} catch (Exception e){
+//			e.printStackTrace();
+//			responder.close();
+//			zmqcontext.term();
+//			
+//		}
+//		responder.close();
+//		zmqcontext.term();
+//	}
 
 
 }
