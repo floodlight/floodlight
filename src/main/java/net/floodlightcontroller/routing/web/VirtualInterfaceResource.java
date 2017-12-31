b71b1347e9ac10012cd032d8312fa726d0b9a696
qingwang <qw@g.clemson.edu>
2017-12-30 23:09:24 -0500

    Okay, REST API part should be closed.

--- src/main/java/net/floodlightcontroller/routing/web/VirtualInterfaceResource.java
+++ src/main/java/net/floodlightcontroller/routing/web/VirtualInterfaceResource.java
@@ -1,30 +1,103 @@
 package net.floodlightcontroller.routing.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.floodlightcontroller.routing.IRoutingService;
+import net.floodlightcontroller.routing.VirtualGateway;
 import net.floodlightcontroller.routing.VirtualGatewayInterface;
-import org.restlet.resource.Delete;
-import org.restlet.resource.Post;
-import org.restlet.resource.Put;
-import org.restlet.resource.ServerResource;
+import org.restlet.resource.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
 
 /**
  * @author Qing Wang (qw@g.clemson.edu) at 12/29/17
  */
 public class VirtualInterfaceResource extends ServerResource {
 
+    @Get
+    public Object getVirtualInterfaces() {
+        IRoutingService routingService =
+                (IRoutingService) getContext().getAttributes()
+                        .get(IRoutingService.class.getCanonicalName());
+
+        String gatewayName = (String) getRequestAttributes().get("gateway-name");
+        String interfaceName = (String) getRequestAttributes().get("interface-name");
+
+        Optional<VirtualGateway> gateway = routingService.getVirtualGateway(gatewayName);
+        if (!gateway.isPresent()) {
+            return Collections.singletonMap("INFO: ", "Virtual gateway " + gatewayName + " not found");
+        }
+        if (routingService.getGatewayInterfaces(gateway.get()).get().isEmpty()) {
+            return Collections.singletonMap("INFO: ", "No virtual interface exists on " + gatewayName + " yet");
+        }
+
+        if (interfaceName.equals("all")) {
+            return routingService.getGatewayInterfaces(gateway.get()).get();
+        }
+        else {
+            return routingService.getGatewayInterface(interfaceName, gateway.get());
+        }
+
+
+    }
+
+    @Delete
+    public Object removeVirtualInterface() throws IOException {
+        IRoutingService routingService =
+                (IRoutingService) getContext().getAttributes()
+                        .get(IRoutingService.class.getCanonicalName());
+
+        String gatewayName = (String) getRequestAttributes().get("gateway-name");
+        String interfaceName = (String) getRequestAttributes().get("interface-name");
+
+        Optional<VirtualGateway> gateway = routingService.getVirtualGateway(gatewayName);
+        if (!gateway.isPresent()) {
+            return Collections.singletonMap("INFO: ", "Virtual gateway " + gatewayName + " not found");
+        }
+        if (routingService.getGatewayInterfaces(gateway.get()).get().isEmpty()) {
+            return Collections.singletonMap("INFO: ", "No virtual interface exists on " + gatewayName + " yet");
+        }
+
+        if (interfaceName.equals("all")) {
+            routingService.removeAllVirtualInterfaces(gateway.get());
+            return Collections.singletonMap("INFO: ", "All virtual interface from" + gatewayName + " removed");
+        }
+        else {
+            if (routingService.removeVirtualInterface(interfaceName, gateway.get())) {
+                return Collections.singletonMap("INFO: ", "Virtual interface" + interfaceName +
+                        " from gateway " + gatewayName + " removed");
+            }
+            else {
+                return Collections.singletonMap("INFO: ", "Virtual interface" + interfaceName +
+                        " from gateway " + gatewayName + "not found");
+            }
+        }
+
+    }
+
     @Put
     @Post
-    // This also includes overwrite an existing interface
-    public void addVirtualInterface(String jsonData) throws IOException {
+    public Object createVirtualInterface(String jsonData) throws IOException {
+        IRoutingService routingService =
+                (IRoutingService) getContext().getAttributes()
+                        .get(IRoutingService.class.getCanonicalName());
 
+        String gatewayName = (String) getRequestAttributes().get("gateway-name");
+        String interfaceName = (String) getRequestAttributes().get("interface-name");
 
-        try {
+        Optional<VirtualGateway> gateway = routingService.getVirtualGateway(gatewayName);
+        if (!gateway.isPresent()) {
+            return Collections.singletonMap("INFO: ", "Virtual gateway" + gatewayName + " not found");
+        }
+
+        try{
             VirtualGatewayInterface vInterface = new ObjectMapper()
                     .reader(VirtualGatewayInterface.class)
                     .readValue(jsonData);
 
+            routingService.createVirtualInterface(gateway.get(), vInterface);
+            return Collections.singletonMap("INFO: ", "Virtual interface " + interfaceName + " created on" + gatewayName );
         }
         catch (IOException e) {
             throw new IOException(e);
@@ -32,11 +105,5 @@ public class VirtualInterfaceResource extends ServerResource {
 
     }
 
-
-    @Delete
-    public void deleteVirtualInterface(String jsonData) throws IOException {
-
-
-    }
 
 }
