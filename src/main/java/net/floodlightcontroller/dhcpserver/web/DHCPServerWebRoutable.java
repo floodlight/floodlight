3b35bacfd495f7ba967093ac6f362d9b5d6040a4
Geddings Barrineau <cbarrin@g.clemson.edu>
2018-02-20 18:45:44 -0500

    Removed the "add" portion of the static binding REST API

--- src/main/java/net/floodlightcontroller/dhcpserver/web/DHCPServerWebRoutable.java
+++ src/main/java/net/floodlightcontroller/dhcpserver/web/DHCPServerWebRoutable.java
@@ -1,7 +1,6 @@
 package net.floodlightcontroller.dhcpserver.web;
 
 import net.floodlightcontroller.restserver.RestletRoutable;
-
 import org.restlet.Context;
 import org.restlet.routing.Router;
 
@@ -15,7 +14,7 @@ public class DHCPServerWebRoutable implements RestletRoutable {
         router.attach("/add/instance/json", InstanceResource.class);
         router.attach("/get/instance/{instance-name}/json", InstanceResource.class);
         router.attach("/del/instance/{instance-name}/json", InstanceResource.class);
-        router.attach("/add/static-binding/json", BindingResource.class);
+        router.attach("/static-binding/json", BindingResource.class);
 
         return router;
     }
